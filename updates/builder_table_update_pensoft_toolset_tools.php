ec504c4649915e15f1b3526b6a99d933aaabde59
root <preprint@pensoft.net>
2026-01-15 15:00:15 +0200

    new fields

---
+++ updates/builder_table_update_pensoft_toolset_tools.php
@@ -0,0 +1,25 @@
+<?php namespace Pensoft\Toolset\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftToolsetTools extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_toolset_tools', function($table)
+        {
+            $table->string('level')->nullable();
+            $table->text('intro_questions')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_toolset_tools', function($table)
+        {
+            $table->dropColumn('level');
+            $table->dropColumn('intro_questions');
+        });
+    }
+}
